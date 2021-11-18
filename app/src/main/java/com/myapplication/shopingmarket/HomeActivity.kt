5c45f52f5714f1b59125b35bf50e3631561348cf
CClaraM <cristiancamilolara@hotmail.com>
2021-11-18 18:00:27 -0500

    Create last-build.bin

--- app/src/main/java/com/myapplication/shopingmarket/HomeActivity.kt
+++ app/src/main/java/com/myapplication/shopingmarket/HomeActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlin.concurrent.thread
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(2000)
+        setTheme(R.style.Theme_ShopingMarket)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
     }
