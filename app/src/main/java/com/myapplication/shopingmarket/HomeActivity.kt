a308434c7e9d2ada5e6e832f4f734a8288dd0c79
CClaraM <cristiancamilolara@hotmail.com>
2021-11-18 20:31:43 -0500

    Create last-build.bin

--- app/src/main/java/com/myapplication/shopingmarket/HomeActivity.kt
+++ app/src/main/java/com/myapplication/shopingmarket/HomeActivity.kt
@@ -3,9 +3,11 @@ package com.myapplication.shopingmarket
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
-//import kotlin.concurrent.thread
+import kotlin.concurrent.thread
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,12 +15,29 @@ class HomeActivity : AppCompatActivity() {
         setTheme(R.style.Theme_ShopingMarket)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setSupportActionBar(findViewById(R.id.mainToolbar))
         showToast(1)
     }
 
-    fun logOn(botonLogin: View) {
-        val intent = Intent(this,LoginActivity::class.java)
-        startActivity(intent)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when(item.itemId) {
+        R.id.action_search ->{
+            true
+        }
+        R.id.login_btn ->{
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            true
+        }
+
+        else ->{
+            super.onOptionsItemSelected(item)
+        }
+
     }
 
     fun showToast(int: Int){
