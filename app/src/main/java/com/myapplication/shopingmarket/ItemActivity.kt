d73dcef34a8e3a7fbabd605f2d11503cde67a9db
CClaraM <cristiancamilolara@hotmail.com>
2021-12-03 22:38:07 -0500

    *.*

--- app/src/main/java/com/myapplication/shopingmarket/ItemActivity.kt
+++ app/src/main/java/com/myapplication/shopingmarket/ItemActivity.kt
@@ -4,20 +4,26 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_item.*
 
 class ItemActivity : AppCompatActivity(){
-
+    private lateinit var product: ProductEntity
     private var images = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item)
 
+//        product= ProductEntity(intent.getStringExtra("Product").toString(),"yhtdf",false,"wew","we","wewer",(10).toDouble(),(2).toDouble(),"none")
 //        Toast.makeText(applicationContext,intent.getStringExtra("Product"),Toast.LENGTH_LONG).show()  //OnlyTest
+
         createImage()
     }
 
     private fun createImage() {
-
+        images = ArrayList()
+        var sliderAdapter = ItemSliderAdapter(images as ArrayList<String>)
+        item_slider.adapter=sliderAdapter
     }
 }
